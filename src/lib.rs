9ad7a46bfc6dc6249ba7eba117c49a0e8f2ce2d7
j03-dev <24nomeniavo@gmail.com>
2024-11-08 14:42:39 +0300

    change std::io::Reslut to anyhow result

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,7 @@ pub fn action(_attr: TokenStream, item: TokenStream) -> TokenStream {
 
         #[russenger::async_trait]
         impl russenger::Action for #name {
-            async fn execute(&self, res: russenger::prelude::Res, req: russenger::prelude::Req) -> std::io::Result<()> {
+            async fn execute(&self, res: russenger::prelude::Res, req: russenger::prelude::Req) -> anyhow::Result<()> {
                 #body
             }
             fn path(&self) -> String {
