1b7a552736b8ce5644ae6e6f35b0e6594e3085f5
j03-dev <24nomeniavo@gmail.com>
2024-12-31 01:37:27 +0300

    update paradigm

--- src/lib.rs
+++ src/lib.rs
@@ -31,24 +31,22 @@ use syn::{parse_macro_input, ItemFn};
 /// This macro simplifies the process of creating a new action by automatically generating the struct and implementing the `Action` trait for it.
 #[proc_macro_attribute]
 pub fn action(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    // Parse the input function
     let input = parse_macro_input!(item as ItemFn);
-    let name = &input.sig.ident;
 
-    let body = &input.block;
+    // Extract function components
+    let sig = input.sig;
+    let vis = input.vis; // Function visibility
+    let ident = sig.ident; // Function name
+    let inputs = sig.inputs; // Function parameters
+    let block = input.block;
 
+    // Generate a new function with the proper async wrapping
     let expanded = quote! {
-        #[allow(non_camel_case_types)]
-        pub struct #name;
-
-        #[russenger::async_trait]
-        impl russenger::Action for #name {
-            async fn execute(&self, res: russenger::prelude::Res, req: russenger::prelude::Req) -> russenger::error::Result<()> {
-                #body
-            }
-            fn path(&self) -> String {
-                stringify!(#name).to_string()
-            }
+        #vis fn #ident(#inputs) -> std::pin::Pin<Box<dyn std::future::Future<Output = error::Result<()>>>> {
+            Box::pin(async move #block)
         }
     };
+
     TokenStream::from(expanded)
 }
